47ec62e946c10e7ca5f0921353b62d7d9213c856
MHamzaaa <b18101069.m.hamzasohail@gmail.com>
2022-03-05 15:56:40 +0500

    24:15

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var storageRef = firebase.storage().ref("/Images/my-pics.jpg");
 var inputImage = document.getElementById('chooseFile');
 var uploadBtn = document.getElementById('uploadBtn');
+var db = firebase.firestore();
+var imageList = [];
 var fileData;
 
 inputImage.addEventListener('change', (e) => {
@@ -9,6 +11,7 @@ inputImage.addEventListener('change', (e) => {
 
 uploadBtn.addEventListener('click' , (e) => {
     uploadImg();
+    fileData = "";
 })
 
 
@@ -21,14 +24,74 @@ var uploadImg = async () => {
         var data = await storageRef.put(fileData);
         let url = await data.ref.getDownloadURL();
         console.log(url);
+        imageList.push(url);
+        refreshUI();
+        loadSingleImage(url);
+        
+        var data = {
+            images : imageList
+        }
 
         document.getElementById('svg').style.display = "none";
+        inputImage.value = "";
+        var imageTag = document.getElementById('img');
+        imageTag.src = url;
 
-        document.getElementById('img').src = url;
+        await updateDocument(data);
+        
     } catch (error) {
         console.log(error);
         document.getElementById('svg').style.display = "none";
     }
 }
 
+var createDocumentInDataBase = async (data) => {
+    try {
+        await db.collection("Images").doc("image-list").set(data);
+        console.log("document successfully created");
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+var readDocumentById = async () => {
+    try {
+        var document = await db.collection("Images").doc("image-list").get();
+        return document.data();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+var updateDocument = async (data) => {
+    try {
+        var document = await db.collection("Images").doc("image-list").set(data, {merge : true});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function refreshUI () {
+    var list = document.querySelector('.imgCont');
+    list.innerHTML = '';
+    for (let i = 0; i < imageList.length; i++) {
+        list.innerHTML += `<img id="img" src="${imageList[i]}" >` ;
+    }
+}
+
+function loadSingleImage(){
+    var list = document.querySelector('.list');
+    let ele = document.createElement('img');
+    ele.src = url;
+    ele.classList.add("image");
+    list.appendChild(ele);
+}
+
+Start();
+
+async function Start () {
+    var data = await readDocumentById();
+    imageList = data.images;
+    refreshUI();
+}
